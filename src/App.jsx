f0c057d02c99492f0c309bbee563bc0571058cb7
Akash Deep Gupta <134071390+Akashdeepgupta124@users.noreply.github.com>
2025-07-23 23:11:13 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import "./App.css";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
+import Index from "./pages/index";
+import NotFound from "./pages/notfound";
 
 const App = () => (
   <BrowserRouter>
